dc05d72e17b8104a5d79b65589e9307d085747be
zane <zzg9583@gmail.com>
2022-08-17 10:59:31 +0800

    fix

--- weiboHotList.py
+++ weiboHotList.py
@@ -3,14 +3,16 @@ import requests
 import json
 import pymongo
 from account import db_name, host, port, name, pwd
+import pydash as _
 
 
 class Hot:
-    def __init__(self, now_time=None, log=None):
+    def __init__(self, now_time=None, log=None, retry=0):
         client = pymongo.MongoClient(host=host, port=port, username=name, password=pwd)
         self.my_db = client[db_name]
         self.now_time = now_time
         self.log = log
+        self.retry = retry
 
     def run(self):
         self.__req(url=config.get_hot_list_url())
@@ -21,10 +23,13 @@ class Hot:
             res_pones = requests.get(url, headers=header, cookies={})
             if 200 == res_pones.status_code:
                 data = json.loads(str(res_pones.text))
-                for item in data['data']['cards'][0]['card_group']:
-                    item['created_at'] = self.now_time
-                    self.my_db.hot_list.insert_one(item)
-                    pass
+                group = _.get(data, 'data.cards[0].card_group', [])
+                if len(group):
+                    self.__extract(group)
+                elif self.retry < 5:
+                    self.run()
+                else:
+                    self.log('not data ----->   ' + str(res_pones.text), self.now_time)
             else:
                 print("request error", res_pones.status_code)
                 self.log("request error    " + str(res_pones.status_code) + "    " + self.now_time)
@@ -32,6 +37,12 @@ class Hot:
             print("err1-->", err)
             self.log("err1-->    " + str(err) + "    " + self.now_time)
         pass
+
+    def __extract(self, data):
+        for item in data:
+            item['created_at'] = self.now_time
+            self.my_db.hot_list.insert_one(item)
+            pass
 
 
 if __name__ == '__main__':
